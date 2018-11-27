8589463d8de4a1e063ef1b4991dcce0a6de32c07
BoazR <boaz.rymland@forescout.com>
2018-11-27 14:26:04 +0200

    all products page finally works - inc link to product page. styling is not polished

--- src/components/AllProducts.vue
+++ src/components/AllProducts.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="all-products">
         <h1>Our Must-Have Products!</h1>
-        <b-card v-for="product in products" :key="product.id" :title="product.title"
-                :img-src="product.imageUrl"
-                :img-alt="product.title"
-                img-top
-                tag="article"
-                style="max-width: 20rem;"
-                class="mb-2">
-            <p class="card-text">
-                {{product.description}}
-            </p>
-            <b-button href="#" variant="primary" @click="goToProductPage(product, $event)">Details...</b-button>
-        </b-card>
+        <b-card-group deck class="m-3">
+            <b-card v-for="product in products" :key="product.id" :title="product.title"
+                    :img-src="product.images[0]"
+                    :img-alt="product.title"
+                    img-top
+                    tag="article"
+                    style="min-width: 25rem;"
+                    class="mb-2"
+                    bg-variant="light">
+                <p class="card-text" v-html="product.descriptionHtml"></p>
+                <b-button variant="primary" @click="goToProduct(product, $event)">More</b-button>
+            </b-card>
+        </b-card-group>
     </div>
 </template>
 
@@ -23,29 +24,18 @@
         name: 'all-products',
         data: function () {
             return {
-                products: [
-                    {
-                        title: "First product",
-                        description: "The description of the first product",
-                        imageUrl: "aaa"
-                    },
-                    {
-                        title: "Second product",
-                        description: "The description of the second product",
-                        imageUrl: "aaa"
-                    },
-                ]
+                products: []
             }
         },
-        created: () => {
-            // products: getProducts();
-            // prepare all products to show
-            //localStorage.getItem();
-
+        mounted: async function () {
+            // load all products, async'ly
+            this.products = await getProducts();
         },
-        goToProductPage: (product, ev) => {
-            // change route to the product page.
-            this.$router.push({path: `/product/${product.id}`})
+        methods: {
+            goToProduct(product, ev) {
+                // change route to the product page.
+                this.$router.push({name: 'ProductPage', params: {id: product.id}})
+            }
         }
     }
 </script>
