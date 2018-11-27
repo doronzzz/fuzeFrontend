86292edebc6990546f292c0fddbd408ca8210750
doronzzz <gourdoron@gmail.com>
2018-11-27 11:19:55 +0200

    route fix

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,7 +1,14 @@
 <template>
 	<div class="product">
-		<h1>Hello From Product! {{ $route.param.id }}</h1>
+		<h1>Hello From Product! {{ $route.params.id }}</h1>
 		<p>
 		</p>
 	</div>
 </template>
+
+
+<script>
+	// export default {
+	// 	name: 'Product'
+	// }
+</script>
