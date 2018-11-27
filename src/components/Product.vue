919410f9467d963b9ecafeb5b1383a6a21dec780
Evyatar Rosner <evyatar1919@gmail.com>
2018-11-27 16:20:46 +0200

    product loading and mobile compat.

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,12 +1,14 @@
 <template>
-	<b-container class="mt-4 mb-4">
-
+	<b-container v-if="! product.vendor">
+		<b-row align-h="center" class="mt-4 mb-4"><font-awesome-icon icon="spinner" /></b-row>
+	</b-container>
+	<b-container class="mt-4 mb-4" v-else>
 		<h1 class="h3">{{ product.title }}</h1>
 		<b-row class="mt-3">
-			<b-col md="5">
+			<b-col md="5" class="mb-3">
 				<img v-bind:src="selectedImage" class="product__image">
 				<b-row class="product__images mt-2">
-					<b-col md="6" lg="4" v-if="product.images.length > 1" v-for="image in product.images">
+					<b-col cols="4" v-if="product.images.length > 1" v-for="image in product.images">
 						<img v-bind:src="image" class="product__images__image" @click="selectImage(image)" />
 					</b-col>
 				</b-row>
@@ -17,12 +19,14 @@
 					{{ product.vendor }}
 				</div>
 				<div class="product__description mt-2" v-html="product.descriptionHtml"></div>
-				<div class="product__actions mt-4">
-					<b-button @click="addToCart"><font-awesome-icon icon="cart-plus" /> Add To Cart</b-button>
-				</div>
+				<b-container class="mt-4">
+					<b-row align-h="start">
+						<b-col xs="6" sm="5" md="3" class="product__price h4">${{ product.price }}</b-col>
+						<b-col md="6"><b-button @click="addToCart"><font-awesome-icon icon="cart-plus" /> Add To Cart</b-button></b-col>
+					</b-row>
+				</b-container>
 			</b-col>
 		</b-row>
-
 	</b-container>
 </template>
 
@@ -87,6 +91,10 @@
 		@extend .product__image;
 		margin-top: 10px;
 		cursor: pointer;
+	}
+
+	.product__price {
+		padding: 6px;
 	}
 
 </style>
