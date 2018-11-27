c725f59ab4c887c952d2933b73a18ef8ad1e2df4
doronzzz <gourdoron@gmail.com>
2018-11-27 13:52:06 +0200

    cards

--- src/components/CardDeck.vue
+++ src/components/CardDeck.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card-deck-item" style="width: 18rem; background:yellow;">
+    <div class="card-deck-item">
         <vue-swing v-bind:key="product.id" v-for="product in products" @throwout="throwout" @throwin="throwin" :config="config">
             <div class="box">Throw me!</div>
         </vue-swing>
@@ -20,8 +20,37 @@
                     {
                         title: "Second product",
                         description: "The description of the second product"
-                    }]
+                    }],
+                config:{}
             }
-        }
+        },
+        methods:{
+            throwout:function(index){
+                this.products.splice(index,1); // why is this removing only the last row?
+            },
+            throwin:function(){
+                this.products.splice(index,1); // why is this removing only the last row?
+            },
+        },
     }
 </script>
+
+
+<style scoped lang="scss">
+	.card-deck-item .box{
+        background:red;
+        width:350px;
+        height:450px;
+    }
+    .card-deck-item {
+        position: relative;
+        display: block;
+        margin: 0px auto;
+        width: 350px;
+        height: 450px;
+    }
+
+    .card-deck-item > div {
+        position: absolute;
+    }
+</style>
