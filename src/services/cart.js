016f87c5e0694210a07a3fd509bd7b35b83eb006
eavichay <eavichay@gmail.com>
2018-11-27 11:54:55 +0200

    cart

---
+++ src/services/cart.js
@@ -0,0 +1,70 @@
+// @ts-check
+
+/**
+ * @module cart
+ */
+
+/**
+ * @typedef {import('./shop.js').ProductItem} ProductItem
+ */
+
+
+/**
+ * @typedef Cart
+ * @property {ProductItem[]} items
+ */
+
+
+ /**
+  * @type Cart
+  */
+const cart = {
+  items: []
+};
+
+loadFromCache();
+
+function loadFromCache () {
+  const cache = localStorage.getItem('shopify-cart');
+  if (cache) {
+
+    /** @type Cart */
+    const raw = JSON.parse(cache);
+    cart.items = raw.items;
+  } else {
+    saveToCache();
+  }
+}
+
+function saveToCache () {
+  localStorage.setItem('shopify-cart', JSON.stringify(cart));
+}
+
+/**
+ * @return ProductItem[]
+ */
+export const getItems = () => {
+  return cart.items;
+}
+
+/**
+ * @param {ProductItem} productItem 
+ */
+export const addItem = (productItem) => {
+  cart.items.push(productItem);
+  saveToCache();
+}
+
+/**
+ * @param {ProductItem} productItem 
+ */
+export const removeItem = (productItem) => {
+  cart.items = cart.items.filter(product => product.id !== productItem.id);
+  saveToCache();
+}
+
+
+export const clearAll = () => {
+  cart.items = [];
+  saveToCache();
+}
