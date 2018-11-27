dda70c94e63fd83a01df8a790be116eeac1e9c26
eavichay <eavichay@gmail.com>
2018-11-27 10:50:31 +0200

    products fetch

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+// @ts-check
+
+// @ts-ignore
 import Vue from 'vue'
+// @ts-ignore
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/main.scss';
+import { getProducts } from './services/shop';
 
 
 Vue.config.productionTip = false
 
+// @ts-ignore
 new Vue({
 	router,
 	store,
